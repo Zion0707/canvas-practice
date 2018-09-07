51c3d4cdd7cf4bf9d54381239591a7d69df256f2
Zion0707 <zion19920707@gmail.com>
2018-09-07 11:52:03 +0800

    change

--- tween/js/init.js
+++ tween/js/init.js
@@ -10,9 +10,9 @@ $(function(){
     stage = new createjs.Stage("demoCanvas");
     //创建一个形状的显示对象
     circle = new createjs.Shape();
-    circle.graphics.beginFill("red").drawCircle(0, 0, 40);
+    circle.graphics.beginFill("red").drawCircle(0, 0, 10);
     //形状实例的设置位置
-    circle.x = circle.y = 50;
+    circle.x = circle.y = 10;
     //添加形状实例到舞台显示列表
     stage.addChild(circle);
     //更新舞台将呈现下一帧
