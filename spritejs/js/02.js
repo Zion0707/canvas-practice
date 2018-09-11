f06126f51636f02933fef5b776a71c915fae597e
Zion0707 <zion19920707@gmail.com>
2018-09-11 18:45:03 +0800

    change

--- spritejs/js/02.js
+++ spritejs/js/02.js
@@ -1,32 +1,29 @@
 $(function(){
 
-    const {Scene, Sprite} = spritejs;
+    const { Scene, Group, Sprite} = spritejs;
 
     const scene = new Scene('#container', {
         resolution: [750, 2668],
     })
-    const sprite1 = new Sprite()
-    sprite1.attr({
-        pos: [100, 100],
-        size: [100, 100],
-        bgcolor: 'red',
-        border:{
-            style:'dashed',
-            width: 1,
-            color: '#37c',
-        }
-    });
-    const sprite2 = new Sprite()
-    sprite2.attr({
-        pos: [200, 200],
-        size: [100, 100],
-        bgcolor: 'red',
-        border: {
-            style: 'dashed',
-            width: 1,
-            color: '#37c',
-        }
+
+    const layer = scene.layer('fglayer')
+    const group = new Group()
+
+    group.attr({
+        anchor: 0.5,
+        pos: [300, 300],
+        size: [200, 200],
+        clip:{}
     })
-    scene.layer().append(sprite1, sprite2);
+
+
+    const sprite = new Sprite('https://p4.ssl.qhimg.com/t01423053c4cb748581.jpg')
+    sprite.attr({
+        pos: [-10, 0],
+        scale: 0.6,
+    })
+
+    layer.append(group);
+
 
 });
