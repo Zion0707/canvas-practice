859aefa5914916525e1c9c27177d3a1319ff906a
Zion0707 <zion19920707@gmail.com>
2018-09-11 09:45:28 +0800

    change

--- createjs/js/05.js
+++ createjs/js/05.js
@@ -1,13 +1,8 @@
 $(function () {
-    // canvas自适应
     var canvas = $('#canvas');
-    canvas.attr('width', $(window).width());
-    canvas.attr('height', $(window).height());
-    $(window).on('resize', function () {
-        canvas.attr('width', $(window).width());
-        canvas.attr('height', $(window).height());
-    });
+    canvas.attr('width', 750);
+    canvas.attr('height', 2668);
 
-    
+    var stage = new createjs.Stage('canvas');
     
 });
