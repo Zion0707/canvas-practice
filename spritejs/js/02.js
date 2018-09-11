4b3e32ee193d432fc38ae95e67bdc5a7d3114d19
Zion0707 <zion19920707@gmail.com>
2018-09-11 18:57:11 +0800

    change

--- spritejs/js/02.js
+++ spritejs/js/02.js
@@ -13,15 +13,19 @@ $(function(){
         anchor: 0.5,
         pos: [300, 300],
         size: [200, 200],
-        clip:{}
+        clip:{
+            d:'m29.5,144l395.5,1l-94,189l-301.5,-190z'
+        }
     })
 
 
     const sprite = new Sprite('https://p4.ssl.qhimg.com/t01423053c4cb748581.jpg')
     sprite.attr({
-        pos: [-10, 0],
-        scale: 0.6,
+        pos: [0, 100],
+        scale: 1,
     })
+
+    group.append(sprite);
 
     layer.append(group);
 
