5fe27ae9c1c3dc7ead8e48dab540e33a1ae39e26
Zion0707 <zion19920707@gmail.com>
2018-09-11 14:34:00 +0800

    change

--- createjs/js/05.js
+++ createjs/js/05.js
@@ -1,8 +1,46 @@
 $(function () {
-    var canvas = $('#canvas');
-    canvas.attr('width', 750);
-    canvas.attr('height', 2668);
 
+    var canvas = $('#canvas');
+    canvas.attr({ 'width': 750, 'height': 2668 });
+    
     var stage = new createjs.Stage('canvas');
+    
+    // 显示区
+    var displayArea = new createjs.Container();
+    displayArea.set({ name:'displayArea' });
+    //背景
+    var daBg = new createjs.Shape();
+    daBg.graphics.beginFill('#f1f1f1').drawRect(0, 0, 750 , 1000);
+    daBg.set({ name:'daBg' });
+
+    //显示区div
+    displayArea.addChild(daBg);
+    
+    
+    // 操作区
+    var toolArea = new createjs.Container();
+    toolArea.set({ name:'toolArea' });
+    var taBg = new createjs.Shape();
+    taBg.graphics.beginFill().drawRect(0, 1000, 750, 334);
+    taBg.set({ name: 'taBg' });
+    toolArea.addChild(taBg);
+    
+
+
+
+
+    stage.addChild( displayArea , toolArea );
+
+    //实时更新
+    createjs.Ticker.addEventListener("tick", handleTick);
+    function handleTick(event) {
+        if (!event.paused) {
+            stage.update();
+        }
+    }
+    //触屏
+    createjs.Touch.enable(stage);
+
+
     
 });
