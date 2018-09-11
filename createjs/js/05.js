a52078a0dd99cb35befcc4a4b44c071cd2e3132c
Zion0707 <zion19920707@gmail.com>
2018-09-11 09:13:53 +0800

    change

--- createjs/js/05.js
+++ createjs/js/05.js
@@ -9,41 +9,5 @@ $(function () {
     });
 
     
-    var stage = new createjs.Stage('canvas');
-    stage.set({name:'canvas'});
-
-
-    var container = new createjs.Container();
-    container.set({ alpha:0.5 });
-
-    var shape1 = new createjs.Shape();
-    shape1.graphics.beginFill('orange').drawRect(0,0,100,100);
-    shape1.set({ name:'shape1' });
-
-    // shape1.on('click',function(ev){
-    //     let parent = ev.target.parent;
-    //     stage.removeChild(parent);
-    // });
-
-    var shape2 = new createjs.Shape();
-    shape2.graphics.beginFill('gray').drawRect(100,100,100,100);
-    shape2.set({ name:'shape2' });
-
-
-    stage.addEventListener('click',clickEle);
-
-    function clickEle(ev){
-        el = ev.target.name;
-        console.log(el);
-    }
-
-
-    container.addChild(shape1, shape2);
-    stage.addChild(container);
-    // stage.update();
-    createjs.Ticker.on('tick', handleTick);
-    function handleTick(){
-        stage.update();
-    }
-    createjs.Touch.enable(stage);
+    
 });
