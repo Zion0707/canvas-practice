055567a3467d463acb4f1213d8c06dc803ae52cd
Zion0707 <zion19920707@gmail.com>
2018-09-17 16:07:12 +0800

    change

--- spritejs/js/07.js
+++ spritejs/js/07.js
@@ -3,8 +3,24 @@ $(function(){
     const scene = new Scene('#container',{
         resolution: [750, 2668],
     });
+    const div1 = new Group();
+    const div1_s1 = new Sprite();
+    div1_s1.attr({
+        size:[50,50],
+        pos:[0,0],
+        bgcolor:'orangered'
+    });
+    
+    div1_s1.animate([{ x: 200, y: 200 }], {
+        duration: 1000,
+        iterations: Infinity,
+    });
+        
+    div1.append(div1_s1);
+    scene.layer().append(div1);
+    
 
 
-    scene.layer().append();
+
 
 });
