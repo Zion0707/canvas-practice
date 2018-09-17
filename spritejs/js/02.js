82d60e9374bf87dd967a6cfe75731185ccdc46d1
Zion0707 <zion19920707@gmail.com>
2018-09-17 14:23:28 +0800

    change

--- spritejs/js/02.js
+++ spritejs/js/02.js
@@ -1,29 +1,30 @@
 $(function(){
+    // svg 路径生成器 http://www.zuohaotu.com/svg/
 
     const { Scene, Group, Sprite} = spritejs;
 
     const scene = new Scene('#container', {
         resolution: [750, 2668],
-    })
+    });
 
-    const layer = scene.layer('fglayer')
-    const group = new Group()
+    const layer = scene.layer('fglayer');
+    const group = new Group();
 
     group.attr({
-        anchor: 0.5,
-        pos: [300, 300],
+        anchor: 0,
+        pos: [0, 0],
         size: [200, 200],
         clip:{
-            d:'m29.5,144l395.5,1l-94,189l-301.5,-190z'
+            d:'m3.500006,99.437495c0,-53.038673 42.961325,-95.999998 95.999998,-95.999998c53.038673,0 95.999998,42.961325 95.999998,95.999998c0,53.038673 -42.961325,95.999998 -95.999998,95.999998c-53.038673,0 -95.999998,-42.961325 -95.999998,-95.999998z',
         }
-    })
+    });
 
-
-    const sprite = new Sprite('https://p4.ssl.qhimg.com/t01423053c4cb748581.jpg')
+    //图片载入及x,y轴位置调整及可放大缩小设置
+    const sprite = new Sprite('https://p4.ssl.qhimg.com/t01423053c4cb748581.jpg');
     sprite.attr({
-        pos: [0, 100],
+        pos: [-50, -50],
         scale: 1,
-    })
+    });
 
     group.append(sprite);
 
