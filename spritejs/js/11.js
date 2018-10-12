8027dab4972ce0731c29647b8644ca7c57f77936
Zion0707 <zion19920707@gmail.com>
2018-10-12 15:15:44 +0800

    change

--- spritejs/js/11.js
+++ spritejs/js/11.js
@@ -1,5 +1,55 @@
 $(function(){
 
+    const { Scene, Sprite, Group, Label } = spritejs;
+    const scene = new Scene('#container',{
+        resolution: [750, 2668],
+    });
+    const layer = scene.layer();
+
+    //随机范围数
+    function rd(n,m){
+        var c = m-n+1; 
+        return Math.floor(Math.random() * c + n);
+    }
+
+    // 创建下落物体
+    var createBall = function(){
+        var ball = new Sprite('../images/11.png');
+        ball.attr({
+            size:[100,100],
+            pos:[ rd(0,750) , -100],
+            bgcolor:'#ff5722',
+            borderRadius:50,
+            anchor:[0.5,0.5]
+        });
+        layer.append(ball);    
+        ball.animate([
+            { y: -100 , rotate:0 },
+            { y: 2668 , rotate: rd(100,10000)},
+        ],{
+            duration: rd(5000,10000),
+            iterations: 1,
+        });
+    }
+
+    //定时下落
+    setInterval(()=>{
+        createBall();
+    },1000);
+
+
+
+
+
+
+
+
+
     
-    
+
+
+
+
+
+
 });
---
+++ spritejs/js/12.js
@@ -0,0 +1,32 @@
+$(function(){
+
+    const { Scene, Sprite, Group, Label } = spritejs;
+    const scene = new Scene('#container',{
+        resolution: [750, 2668],
+    });
+    const layer = scene.layer();
+
+    const sprite = new Sprite('https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=589517029,792426897&fm=26&gp=0.jpg');
+    sprite.attr({
+        pos:[100,100],
+        size:[200,200],
+        borderRadius:100
+    });
+    layer.append(sprite);    
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+});
